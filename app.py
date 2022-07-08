2b663562eaccb88f6c29c2d4af8a786b2990f544
Anagha-17 <105308859+Anagha-17@users.noreply.github.com>
2022-07-08 18:42:26 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import joblib
+model =joblib.load('train.csv')
+st.title('PREDICT THE TYPE')
+ip=st.text_input('ENTER THE SONG')
+op= model.predict([ip])
+if st.button('PREDICT'):
+ st.title(op[0])
